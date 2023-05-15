32dbaa6d9cae21e881c5a383d759d9d49cdcf927
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-05-15 18:56:46 +0000

    added vercel deployment settings

--- cookie_stand_project/wsgi.py
+++ cookie_stand_project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cookie_stand_project.settings")
 
 application = get_wsgi_application()
+
+# This is a vercel thing
+app = application
